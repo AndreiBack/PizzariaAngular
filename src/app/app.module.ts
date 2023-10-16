fa43014eb5bfcb426a0c62640895fa3c3a7918cb
AndreiBack <andreigregoriback@gmail.com>
2023-10-16 19:03:06 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PizzaListComponent } from './components/pizza/pizza-list/pizza-list.component';
+import { PizzaDetailsComponent } from './components/pizza/pizza-details/pizza-details.component';
+import { SaborDetailsComponent } from './components/sabor/sabor-details/sabor-details.component';
+import { SaborListComponent } from './components/sabor/sabor-list/sabor-list.component';
+import { ClienteDetailsComponent } from './components/cliente/cliente-details/cliente-details.component';
+import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
+import { ProdutoListComponent } from './components/produto/produto-list/produto-list.component';
+import { ProdutoDetailsComponent } from './components/produto/produto-details/produto-details.component';
+import { PedidoListComponent } from './components/pedido/pedido-list/pedido-list.component';
+import { PedidoDetailsComponent } from './components/pedido/pedido-details/pedido-details.component';
+import { FuncionarioListComponent } from './components/funcionario/funcionario-list/funcionario-list.component';
+import { FuncionarioDetailsComponent } from './components/funcionario/funcionario-details/funcionario-details.component';
+import { EnderecoListComponent } from './components/endereco/endereco-list/endereco-list.component';
+import { EnderecoDetailsComponent } from './components/endereco/endereco-details/endereco-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PizzaListComponent,
+    PizzaDetailsComponent,
+    SaborDetailsComponent,
+    SaborListComponent,
+    ClienteDetailsComponent,
+    ClienteListComponent,
+    ProdutoListComponent,
+    ProdutoDetailsComponent,
+    PedidoListComponent,
+    PedidoDetailsComponent,
+    FuncionarioListComponent,
+    FuncionarioDetailsComponent,
+    EnderecoListComponent,
+    EnderecoDetailsComponent
   ],
   imports: [
     BrowserModule,
